df32c7dede8c72ad060496f3039c028cdb69728e
Prathipati Mohith <142586455+mohith-prathipati@users.noreply.github.com>
2026-07-24 11:22:01 +0530

    Add root_path to FastAPI initialization

--- backend /main.py
+++ backend /main.py
@@ -11,7 +11,7 @@ from fastapi import FastAPI, UploadFile, File, Form, HTTPException, BackgroundTa
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, StreamingResponse
 
-app = FastAPI(title="Codiq Engine API", version="2.0.0")
+app = FastAPI(title="Codiq Engine API", version="2.0.0,root_path='/api")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://localhost:3000"],
